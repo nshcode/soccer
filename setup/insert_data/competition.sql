d8e7cf3f827c375819911c582f718ec4c83a3008
nshcode <103318915+nshcode@users.noreply.github.com>
2025-04-14 17:26:58 +0200

    soccer data in form of SQL-insert statments

---
+++ setup/insert_data/competition.sql
@@ -0,0 +1,9 @@
+REM INSERTING into COMPETITION
+SET DEFINE OFF;
+Insert into COMPETITION (COMPETITIONID,NAME,FORMAT,AREAID,TYPE) values (28,'World Cup','International cup',0,'international');
+Insert into COMPETITION (COMPETITIONID,NAME,FORMAT,AREAID,TYPE) values (102,'European Championship','International cup',0,'international');
+Insert into COMPETITION (COMPETITIONID,NAME,FORMAT,AREAID,TYPE) values (364,'English first division','Domestic league',0,'club');
+Insert into COMPETITION (COMPETITIONID,NAME,FORMAT,AREAID,TYPE) values (412,'French first division','Domestic league',250,'club');
+Insert into COMPETITION (COMPETITIONID,NAME,FORMAT,AREAID,TYPE) values (426,'German first division','Domestic league',276,'club');
+Insert into COMPETITION (COMPETITIONID,NAME,FORMAT,AREAID,TYPE) values (524,'Italian first division','Domestic league',380,'club');
+Insert into COMPETITION (COMPETITIONID,NAME,FORMAT,AREAID,TYPE) values (795,'Spanish first division','Domestic league',724,'club');
---
+++ setup/insert_data/playerrole.sql
@@ -0,0 +1,6 @@
+REM INSERTING into PLAYERROLE
+SET DEFINE OFF;
+Insert into PLAYERROLE (ROLEID,NAME,CODE2,CODE3) values (1,'Goalkeeper','GK','GKP');
+Insert into PLAYERROLE (ROLEID,NAME,CODE2,CODE3) values (2,'Defender','DF','DEF');
+Insert into PLAYERROLE (ROLEID,NAME,CODE2,CODE3) values (3,'Midfielder','MD','MID');
+Insert into PLAYERROLE (ROLEID,NAME,CODE2,CODE3) values (4,'Forward','FW','FWD');
