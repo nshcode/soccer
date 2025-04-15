d37bae22044ee76c1cdc6613c6105e196cacfebe
nshcode <103318915+nshcode@users.noreply.github.com>
2025-04-15 20:16:32 +0200

    matchteam view

---
+++ queries/all-series-of-wins.sql
@@ -0,0 +1,26 @@
+SELECT
+  competition, team,
+  from_gameweek, to_gameweek,
+  to_char(from_date, 'DD-MM-YYYY') AS from_dae,
+  to_char(to_date, 'DD-MM-YYYY')   AS to_date,
+  num_of_wins
+FROM(
+  SELECT * FROM matchteam_vw
+  WHERE competition NOT IN ('World Cup', 'European Championship')
+ ) MATCH_RECOGNIZE (
+    PARTITION BY competition, team
+    ORDER BY dateutc
+    MEASURES
+      FIRST(W.dateutc)    AS from_date,
+      E.dateutc           AS to_date,
+      FIRST(W.gameweek)   AS from_gameweek,
+      E.gameweek          AS to_gameweek,
+      COUNT(*)            AS num_of_wins
+    ONE ROW PER MATCH
+    AFTER MATCH SKIP PAST LAST ROW
+    PATTERN (W+ E) 
+    DEFINE
+      W AS W.winner = 'Y' AND NEXT(W.winner) = 'Y'
+ )
+WHERE team = 'Bayern München' ORDER BY from_date;
+
---
+++ views/create_matchteam_vw.sql
@@ -0,0 +1,19 @@
+DROP VIEW matchteam_vw;
+CREATE VIEW matchteam_vw AS
+  SELECT c.competitionid
+    , c.name      AS competition
+    , m.matchid
+    , m.dateutc
+    , m.gameweek
+    , t.teamid
+    , t.name      AS team
+    , mt.winner
+    , mt.score
+    , mt.side
+    , co.coachid
+    , co.lastname AS coach
+  FROM competition c
+    JOIN match     m  ON c.competitionid = m.competitionid
+    JOIN matchteam mt ON mt.matchid      = m.matchid
+    JOIN team      t  ON mt.teamid       = t.teamid
+    JOIN coach     co ON co.coachid      = mt.coachid;
