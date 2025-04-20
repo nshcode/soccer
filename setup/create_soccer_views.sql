eb67f692ba85ba902499dd0afbe07b0a4f7978e4
nshcode <103318915+nshcode@users.noreply.github.com>
2025-04-20 23:19:32 +0200

    create_soccer_views updated

--- setup/create_soccer_views.sql
+++ setup/create_soccer_views.sql
@@ -5,6 +5,7 @@ EXECUTE  DBMS_OUTPUT.PUT_LINE('Creating views ...');
 SET TERM OFF
 
 @@../views/create_matchteam_vw.sql
+@@../views/create_player_substitution_vw
 
 SET TERM ON
 EXECUTE  DBMS_OUTPUT.PUT_LINE('Views created.');
